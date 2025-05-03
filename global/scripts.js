454200888fbf0f56b1ae2f710464237d54bc866e
recomendadorweb <recomendadorwebnet@gmail.com>
2025-05-03 16:50:11 +0200

    fix mobile

--- global/scripts.js
+++ global/scripts.js
@@ -3,14 +3,3 @@ window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'G-3ZN94Y4QHN');
-
-// Menú hamburguesa
-document.addEventListener('DOMContentLoaded', function () {
-  const toggle = document.querySelector('.hamburger');
-  const navLinks = document.querySelector('.nav-links');
-  if (toggle && navLinks) {
-    toggle.addEventListener('click', function () {
-      navLinks.classList.toggle('activo');
-    });
-  }
-});
