6588e0391c04fa8e98acb1606c68ffc479504907
recomendadorweb <recomendadorwebnet@gmail.com>
2025-05-04 01:00:12 +0200

    test

--- global/scripts.js
+++ global/scripts.js
@@ -3,3 +3,12 @@ window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'G-3ZN94Y4QHN');
+
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.querySelector(".hamburger");
+    const navLinks = document.querySelector(".nav-links");
+  
+    hamburger.addEventListener("click", function () {
+      navLinks.classList.toggle("activo");
+    });
+  });
