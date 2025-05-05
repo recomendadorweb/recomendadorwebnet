b8a4006ac278f1537cbcc7414c699ac1b7e9617d
recomendadorweb <recomendadorwebnet@gmail.com>
2025-05-05 08:58:50 +0200

    test

--- global/scripts.js
+++ global/scripts.js
@@ -1,9 +1,3 @@
-// Google Analytics
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-3ZN94Y4QHN');
-
 document.addEventListener("DOMContentLoaded", function () {
     const hamburger = document.querySelector(".hamburger");
     const navLinks = document.querySelector(".nav-links");
